cc19fa6c952ddc9acc802172611d9621812f1681
laiamanda <lai.amanda27@gmail.com>
2024-11-12 12:58:09 -0500

    Add documentation to logout.ts

--- src/routes/auth/logout.ts
+++ src/routes/auth/logout.ts
@@ -2,9 +2,16 @@ import express, { Request, Response } from 'express';
 
 export const logout = express.Router();
 
+/**
+ * GET /auth/logout
+ * @param req the request sent from the client
+ * @param res the response sent back to the client
+ * @param next calls next middleware if the user is authorized
+ */
 logout.get('/auth/logout', async (req, res, next) => {
   req.logout(function(error) {
     if(error) {
+      // If user is unable to logout, then return a 400 error
       return res.status(404).render('error', {code: 400});
     }
     res.redirect('/auth/login');
