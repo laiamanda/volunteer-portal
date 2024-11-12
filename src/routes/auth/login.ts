36178c625f7ea7d82bc3ba90c90811a90d9921d5
laiamanda <lai.amanda27@gmail.com>
2024-11-12 13:03:20 -0500

    Add documentation to login.ts

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -3,13 +3,21 @@ import passport from 'passport';
 
 export const login = express.Router();
 
+/**
+ * GET /auth/login
+ * @param req the request from the client
+ * @param res the response sent back to the client
+ */
 login.get('/auth/login', (req: Request, res: Response) => {
   res.render('./auth/login');
 });
 
+/**
+ * POST /auth/login
+ * @param req the request from the client
+ * @param res the response sent back to the client
+ */
 login.post('/auth/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/auth/login'
-}), (req: Request, res: Response) => {
-  console.log(req.body);
-});
+}));
