a198f0c987ad48d7aade47ff1630dd951704f643
laiamanda <lai.amanda27@gmail.com>
2024-11-12 13:07:07 -0500

    Add documentation to signup page

--- src/routes/auth/sign-up.ts
+++ src/routes/auth/sign-up.ts
@@ -1,14 +1,23 @@
 import express, { Request, Response } from 'express';
 import db from '../../util/database';
-import passport from 'passport';
 import bcrypt from 'bcrypt';
 
 export const signUp = express.Router();
 
+/**
+ * GET /auth/sign-up
+ * @param req the request sent from the client
+ * @param res the response sent back to the client
+ */
 signUp.get('/auth/sign-up', (req: Request, res: Response) => {
   res.render('./auth/sign-up');
 });
 
+/**
+ * POST /auth/sign-up
+ * @param req the request sent from the client
+ * @param res the response sent back to the client
+ */
 signUp.post('/auth/sign-up', async (req: Request, res: Response) => {
   const saltRounds = 10;
 
