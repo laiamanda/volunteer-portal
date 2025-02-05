5c2d7806ccb82ba3f3dd27dd51d231dc6131e962
laiamanda <lai.amanda27@gmail.com>
2025-02-05 13:20:48 -0500

    Remove redirect to login and replace with error page

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -5,7 +5,7 @@ import * as auth from '../../util/auth';
 export const profile = Router();
 
 /**
- * GET /user/profile
+ * GET /user/:userId/profile
  * @param req the request sent from the client
  * @param res the response sent back to the client
  */
@@ -13,29 +13,26 @@ profile.get('/user/:userId/profile', auth.loggedIn, async (req, res) => {
 
   const userId = parseInt(req.params.userId);
 
-  if(req.user) {
-    // Checks if user exists
-    const user = (
-      await db.query(
-        `
-          SELECT * 
-          FROM "accounts"."users"
-          WHERE "id" = $1
-        `,[
-          userId,
-        ]
-      )
-    ).rows[0];
+  // Checks if user exists
+  const user = (
+    await db.query(
+      `
+        SELECT * 
+        FROM "accounts"."users"
+        WHERE "id" = $1
+      `,[
+        userId,
+      ]
+    )
+  ).rows[0];
 
-    console.log(req.user.id);
-    console.log(userId);
-
-    if(user === undefined) {
-      return res.redirect('/auth/login');
-    } else {
-      return res.render('user/profile', {
-        user: user,
-      });
-    }
+  // If the user is undefined
+  if(user === undefined) {
+    // Return a 404 error
+    return res.render('./error', {error: res.status(404)});
+  } else {
+    return res.render('user/profile', {
+      user: user,
+    });
   }
 });
