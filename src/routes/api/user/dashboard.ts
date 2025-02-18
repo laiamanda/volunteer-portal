214bb73817efde41a0d762031688317999191484
laiamanda <lai.amanda27@gmail.com>
2025-02-18 12:07:22 -0500

    Update api/user/dashboard.ts

--- src/routes/api/user/dashboard.ts
+++ src/routes/api/user/dashboard.ts
@@ -1,13 +1,19 @@
 import db from '../../../util/database';
 import express, { Request, Response } from 'express';
+import * as  auth from '../../../util/auth'
 
 export const deleteDashboard = express.Router();
-
-deleteDashboard.delete('/api/user/dashboard/:entry_id', async(req, res) => {
+/** 
+ * DELETE /api/user/dashboard/:entry_id
+ * @param req the request sent from the client
+ * @param res the response sent back to the client
+*/
+deleteDashboard.delete('/api/user/dashboard/:entry_id', auth.loggedIn, async(req, res) => {
   const entryId = req.params.entry_id;
-    await db.query(`
-      DELETE FROM "volunteer_entries"."entries"
-      WHERE "id" = $1
-    `, [entryId]);
-    res.end();
+  // Delete from volunteer entries
+  await db.query(`
+    DELETE FROM "volunteer_entries"."entries"
+    WHERE "id" = $1
+  `, [entryId]);
+  res.end();
 });
