4b2ba36bcc839dcf733d57fef9085bc0d38b7335
laiamanda <lai.amanda27@gmail.com>
2025-05-23 19:52:17 -0400

    Add basic css for password valdation

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -3,34 +3,50 @@ const lowerCaseLetters = /[a-z]/g;
 const upperCaseLetters = /[A-Z]/g;
 const numbers = /[0-9]/g;
 
+// password.onfocus = function() {
+//     $('#message').style.display='block';
+// }
+
 $(document).ready(function(){
     $('#password').keyup(function(){
         const password = this.value;
+
         // Validate length
         if(password.length >= 8) {
-            console.log('Good length');
+            $('#length').removeClass('invalid');
+            $('#length').addClass('valid');
         } else {
-            console.log('Too short');
+            $('#length').removeClass('valid');
+            $('#length').addClass('invalid');
         }
         
         // Validate lowerCase
         if(password.match(lowerCaseLetters)) {
             console.log('It has lowercase');
+            $('#letter').removeClass('invalid');
+            $('#letter').addClass('valid');
         } else {
-            console.log('No lowercase');
+            $('#letter').removeClass('valid');
+            $('#letter').addClass('invalid');
         }
 
         // Validate UpperCase
         if(password.match(upperCaseLetters)) {
-            console.log('It has UpperCase');
+             $('#capital').removeClass('invalid');
+            $('#capital').addClass('valid');
         } else {
-            console.log('No uppercase');
+            $('#capital').removeClass('valid');
+            $('#capital').addClass('invalid');
         }
 
         if(password.match(numbers)) {
             console.log('It has numbers');
+            $('#number').removeClass('invalid');
+            $('#number').addClass('valid');
         } else {
             console.log('No numbers');
+            $('#number').removeClass('valid');
+            $('#number').addClass('invalid');
         }
     });
 }) 
