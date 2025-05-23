1582654bb9bc3fe6cfe22bce3f0a33429a8cc2fb
laiamanda <lai.amanda27@gmail.com>
2025-05-23 15:22:55 -0400

    Validate pasword with lowercase

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -1,8 +1,22 @@
 // alert('Connected');
+const lowerCaseLetters = /[a-z]/g;
+const upperCaseLetters = /[A-Z]/g;
 
 $(document).ready(function(){
     $('#password').keyup(function(){
         const password = this.value;
-        console.log(password);
+        // Validate length
+        if(password.length >= 8) {
+            console.log('Good length');
+        } else {
+            console.log('Too short');
+        }
+        
+        // Validate lowerCase
+        if(password.match(lowerCaseLetters)) {
+            console.log('It has lowercase');
+        } else {
+            console.log('No lowercase');
+        }
     });
-})
+}) 
